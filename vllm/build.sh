a19bc0b628c398b2c865983b6885581063f4c5d7
ikrong <contact@ikrong.com>
2025-04-24 18:03:31 +0800

    fix

--- vllm/build.sh
+++ vllm/build.sh
@@ -1,5 +1,5 @@
 # git clone 某个分支
-git clone -b v0.8.1 --depth 1 https://github.com/zhangjunfang/spring-boot-starter-redis.git ./source
+git clone -b v0.8.1 --depth 1 https://github.com/vllm-project/vllm.git ./source
 cp Dockerfile ./source/Dockerfile
 
 cd ./source 

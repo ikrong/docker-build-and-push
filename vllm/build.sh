8dc6bc85ac9b99ebe0a85bdd85434336fec3f5bd
ikrong <contact@ikrong.com>
2025-04-24 17:58:57 +0800

    add vllm

---
+++ vllm/build.sh
@@ -0,0 +1,9 @@
+# git clone 某个分支
+git clone -b v0.8.1 --depth 1 https://github.com/zhangjunfang/spring-boot-starter-redis.git ./source
+cp Dockerfile ./source/Dockerfile
+
+cd ./source 
+
+docker build -t registry.cn-beijing.aliyuncs.com/ikrong/mirrors:vllm-0.8.1 .
+
+docker push registry.cn-beijing.aliyuncs.com/ikrong/mirrors:vllm-0.8.1
